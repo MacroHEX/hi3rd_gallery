b6a76a59880e2450e863baf045438abf81477cc9
MacroHEX <martin18py@gmail.com>
2024-11-17 20:51:12 -0300

    fix: Fixed autoplay not working

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
     <MainLayout>
       <video
         autoPlay
+        muted
         loop
         src="https://fastcdn.hoyoverse.com/mi18n/bh3_global/m20230317hy14h0glc0/upload/564811a1744ce08ffe2c22abf21fa2cb_8931824403464310512.mp4"></video>
     </MainLayout>
