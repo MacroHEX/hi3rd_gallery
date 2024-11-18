310c7f7feefc83e1b85c94dafe6c8891935609da
MacroHEX <martin18py@gmail.com>
2024-11-17 21:03:05 -0300

    feat: Added responsive for home

--- src/layouts/MainHeader.tsx
+++ src/layouts/MainHeader.tsx
@@ -2,8 +2,10 @@ import {Button} from "@/components/ui/button.tsx";
 
 const MainHeader = () => {
   return (
-    <nav className='flex flex-col sm:flex-row gap-10 justify-between px-10 pb-5 items-center bg-neutral-900'>
-      <img src="/assets/logo.png" alt="logo"/>
+    <nav
+      className='flex flex-col sm:flex-row gap-5 sm:gap-10 justify-between px-6 sm:px-10 py-4 items-center bg-neutral-900 sm:bg-black bg-opacity-100 sm:bg-opacity-90 h-fit sm:h-16 shadow-md fixed w-full top-0 z-50'>
+      <img src="/assets/logo.png" alt="logo"
+           className="h-20 hover:scale-105 transition-all inline-block cursor-pointer active:scale-100"/>
       <ul className='flex gap-4'>
         <li>
           <Button variant='link' className='text-white hover:text-neutral-300 transition-all'>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,11 +3,14 @@ import MainLayout from "@/layouts/MainLayout.tsx";
 const Home = () => {
   return (
     <MainLayout>
-      <video
-        autoPlay
-        muted
-        loop
-        src="https://fastcdn.hoyoverse.com/mi18n/bh3_global/m20230317hy14h0glc0/upload/564811a1744ce08ffe2c22abf21fa2cb_8931824403464310512.mp4"></video>
+      <div className='relative w-full h-full pt-36 sm:pt-0'>
+        <video
+          className='w-full h-full object-cover z-0'
+          autoPlay
+          muted
+          loop
+          src="https://fastcdn.hoyoverse.com/mi18n/bh3_global/m20230317hy14h0glc0/upload/564811a1744ce08ffe2c22abf21fa2cb_8931824403464310512.mp4"></video>
+      </div>
     </MainLayout>
   );
 };
